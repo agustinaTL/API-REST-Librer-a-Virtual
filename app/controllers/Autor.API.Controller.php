f3a83e76c36ebac4b774fe919860638201153de0
Benjamin2710 <132313592+Benjamin2710@users.noreply.github.com>
2023-11-12 17:47:11 -0300

    Update Autor.API.Controller.php
    Arreglo y reutilizacion de autores.controller para API
    
    Signed-off-by: Benjamin2710 <132313592+Benjamin2710@users.noreply.github.com>

--- app/controllers/Autor.API.Controller.php
+++ app/controllers/Autor.API.Controller.php
@@ -1,111 +1,93 @@
 <?php
+
 require_once './app/models/AutorModel.php';
 require_once './app/views/API.view.php';
 require_once './app/controllers/API.controller.php';
 
 //
 class AutorAPIController extends ApiController{
-    protected $model;
+   protected $model;
     
-    function __construct() {
-        parent::__construct();
-        $this->model = new AutorModel();
-    }
-    
-    
-    function showAutores($case=0, $autorNew=null) {
-        
+  function __construct() {
+    parent::__construct();
+    $this->model = new AutorModel();
+   }
 
-        $autores = $this->model->getAutores();
-        if(!empty($autores))
-            $this->view->showAutores($autores, $case, $autorNew);
-        else 
-            
+  function get($params = []) {
+    if(empty($params)){
+      $num = isset($_GET['page']) ? (int)$_GET['page'] : 1;
+      $page = $num > 0? $num : 1;
+      $sort = isset($_GET['sort']) ? $_GET['sort'] : null;
+      $order = isset($_GET['order']) ? $_GET['order'] : 'ASC';
 
+      $orderQuery = '';
+      $acceptedOrders = ['ASC','DESC'];
 
-            $this->view->showError('datos no encontrados');
-    }
-    
-    function showAutorById($id, $case=0) {
-        $autor = $this->model->getAutorById($id);
-        if(!empty($autor))
-            $this->view->showAutor($autor);
-        else 
-            $this->view->showError('datos no encontrados');
-    }
-
-    public function agregarAutor() {
-        AuthHelper::verify();
-        $nombre = $_POST['nombre'];
-        $fecha_nacimiento = $_POST['fecha_nacimiento'];
-        $nacionalidad = $_POST['nacionalidad'];
-        
-        
-
-
-        
-
-
-        if (empty($nombre) || empty($fecha_nacimiento) || empty($nacionalidad)) {
-            $this->view->showError("Complete todos los campos");
-            return;
+      if (isset($sort)) {
+        if ($this->model->autorHasColumn($sort) && in_array(strtoupper($order),$acceptedOrders)) {
+          $orderQuery = 'ORDER BY '.$sort.' '.$order;
+        }else{
+          $this->view->response(['response' => 'Bad Request'],400);
+          return;
         }
-        
-        
-        
-        $id = $this->model->insertAutor($nombre, $fecha_nacimiento, $nacionalidad);
-         
+      }
 
-
-        if ($id) {
-                 $this->showAutores(1);
-        } else {
-                 $this->view->showError("Error");
-        }
+      $autor = $this->model->getAutor($page,$orderQuery);
+      if (isset($autor)) {
+        $this->view->response($autor,200);
+      }else{
+        $this->view->response(['response' => 'Bad Request'],400);
+      }
+    }else{
+      $autor = $this->model->getAutorById($params[':ID']);
+      
+      if (!empty($autor)) {
+        $this->view->response($autor,200);
+      }else{
+        $this->view->response(['response' => 'Not Found'],404);
+      }
     }
-
-    function showAutorAModificar($id){
-        AuthHelper::verify();
-        $autor = $this->model->getAutorById($id);
-        $this->view->showAutorModificar($autor);
-    }
-
-    function modificarAutor($id){
-        AuthHelper::verify();
-        if(isset($_POST['nombre']) && isset($_POST['fecha_nacimiento']) &&
-          !empty($_POST['nombre']) && !empty($_POST['fecha_nacimiento'])){
-            $nombre = $_POST['nombre'];
-            $fecha_nacimiento = $_POST['fecha_nacimiento'];
-            
-            $this->model->modificarAutor($id, $nombre, $fecha_nacimiento);
-            $this->showAutorById($id, 1);
-        }
-        else{
-            $this->view->showError("Error al modificar");
-        }
-    }
-
-    function solicitudEliminarAutor($id) {
-        AuthHelper::verify();
-        $TextosIdAutor=$this->model->getTextosIdByAutorId($id);
-        if (empty($TextosIdAutor)) {
-            $this->eliminarAutor($id);
-        } else {
-            $autor=$this->model->getAutorById($id);
-            $this->showAutores(2, $autor);
-        }
-    }
-    
-    function eliminarAutor($id) {
-        AuthHelper::verify();
-        $copiaAutor=$this->model->getAutorById($id);
-        $this->model->borrarAutorById($id);
-        $autorEliminado=$this->model->getAutorById($id);
-        
-        if (empty($autorEliminado)) {
-            $this->showAutores(3, $copiaAutor);
-        } else {
-            $this->view->showError("No pudo eliminarse");
-        }
-    } 
 }
+function add(){
+  $body = $this->getData();
+  $nombre = $body->nombre;
+  $fecha_nacimiento = $body->fecha_nacimiento;
+  $nacionalidad = $body->nacionalidad;
+  $id = $this->model->addAutor($nombre,$fecha_nacimiento,$nacionalidad);
+
+  if ($id) {
+    $this->view->response(['response' => 'Autor added with id '.$id],200);
+  }else{
+    $this->view->response(['response' => 'Error on adding'],404);
+  }
+}
+
+function delete($params = []){
+  $autor = $this->model->getAutorById($params[':ID']);
+  if ($autor) {
+    $this->model->deleteAutor($params[':ID']);
+    $this->view->response(['response' => 'Autor deleted'],200);
+  }else{
+    $this->view->response(['response' => 'Not Found'],404);
+  }
+}
+
+function update($params = []){
+  $id_autor = $params[':ID'];
+  $autor = $this->model->getAutorById($id_autor);
+
+  if ($autor) {
+    $body = $this->getData();
+    $nombreautor = $body->nombreautor;
+    $apellido = $body->apellido;
+
+    $boolean = $this->model->editAutor($id_autor, $nombreautor,$apellido);
+    if ($boolean) {
+      $this->view->response(['response' => 'autor '.$id_autor.' updated'],200);
+    }else{
+      $this->view->response(['response' => 'Error on updating'],404);
+    }
+  }
+}  
+}
+
