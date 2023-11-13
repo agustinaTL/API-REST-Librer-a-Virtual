27e2f72e039e3bc099c2aa1f1bbac4b561d4e040
Benjamin2710 <132313592+Benjamin2710@users.noreply.github.com>
2023-11-12 22:47:42 -0300

    Update APIRouter.php
    ruteo de autor
    
    Signed-off-by: Benjamin2710 <132313592+Benjamin2710@users.noreply.github.com>

--- APIRouter.php
+++ APIRouter.php
@@ -1,6 +1,8 @@
 <?php
 require_once 'libs/Router.php';
+require_once './app/controllers/LibrosAPIcontroller.php';
 
+require_once './app/controllers/Autor.API.controller.php';
 // crea el router
 $router = new Router();
 
@@ -9,6 +11,12 @@ $router->addRoute('libros', 'GET', 'LibrosApiController', 'obtenerLibros');
 $router->addRoute('libros', 'POST', 'LibrosApiController', 'crearLibro');
 $router->addRoute('libros/:ID', 'GET', 'LibrosApiController', 'obtenerLibro');
 $router->addRoute('libros/:ID', 'PUT', 'LibrosApiController', 'modificarDatos');
+
+$router->addRoute('autor/','GET','autorAPIController','get');
+$router->addRoute('autor/:ID','GET','autorAPIController','get');
+$router->addRoute('autor','POST','autorAPIController','add');
+$router->addRoute('autor/:ID','DELETE','autorAPIController','delete');
+$router->addRoute('autor/:ID','PUT','autorAPIController','update');
 
 // rutea
 $router->route($_GET["resource"], $_SERVER['REQUEST_METHOD']);
