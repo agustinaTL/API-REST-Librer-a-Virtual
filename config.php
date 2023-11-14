b4255ff51f9bae14b035cd2303005b4f8b6348cb
agustinaTL <143549252+agustinaTL@users.noreply.github.com>
2023-11-13 23:23:51 -0300

    Update config.php
    Signed-off-by: agustinaTL <143549252+agustinaTL@users.noreply.github.com>

--- config.php
+++ config.php
@@ -2,10 +2,8 @@
 const MYSQL = array(
   'USER' => 'root',
   'PASS' => '',
-<<<<<<< HEAD
   'DB' => 'libreria2.sql',
-=======
   'DB' => 'libreria2',
->>>>>>> 260a19b41466907d1780204a7fd884d4431d4c23
   'HOST' => 'localhost',
+  'JWT_KEY' => 'TPE3Web2!!%'
 );
